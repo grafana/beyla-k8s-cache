b058ae1d42a37731f62c534fba737cf3ce376c04
Mario Macias <mario.macias@grafana.com>
2024-10-28 11:29:20 +0100

    Add basic pseudo-integration testing (#1)
    * Make linting code to pass
    
    * moving the service initialization from main to its own type
    
    * working integration test
    
    * use normal go tests instead of ginkgo
    
    * basic unit test
    
    * end signal sugmission

--- cmd/k8s-cache-client/main.go
+++ cmd/k8s-cache-client/main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
+	"log/slog"
+	"os"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -12,32 +13,35 @@ import (
 )
 
 const (
-	address = "localhost:8999"
+	address = "localhost:50055"
 )
 
+// simple example program that shows how to connect a gRPC+protobuf client to the Kube API cache
 func main() {
 	// Set up a connection to the server.
 	conn, err := grpc.NewClient(address,
 		grpc.WithTransportCredentials(insecure.NewCredentials()))
 
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		slog.Error("could not connect", "address", address, "error", err)
+		os.Exit(-1)
 	}
-	defer conn.Close()
-
 	client := informer.NewEventStreamServiceClient(conn)
 
 	// Subscribe to the event stream.
 	stream, err := client.Subscribe(context.TODO(), &informer.SubscribeMessage{})
 	if err != nil {
-		log.Fatalf("could not subscribe: %v", err)
+		slog.Error("could not subscribe", "error", err)
+		_ = conn.Close()
+		os.Exit(-1)
 	}
+	defer conn.Close()
 
 	// Receive and print messages.
 	for {
 		event, err := stream.Recv()
 		if err != nil {
-			log.Printf("error receiving message: %v", err)
+			slog.Error("receiving message", "error", err)
 			break
 		}
 		fmt.Printf("Received event: %v\n", event)
--- cmd/k8s-cache/main.go
+++ cmd/k8s-cache/main.go
@@ -2,96 +2,37 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"log"
 	"log/slog"
-	"net"
 	"os"
 	"strconv"
 	"time"
 
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/peer"
-
-	"github.com/grafana/beyla-k8s-cache/pkg/informer"
 	"github.com/grafana/beyla-k8s-cache/pkg/meta"
+	"github.com/grafana/beyla-k8s-cache/pkg/service"
 )
 
 const defaultPort = 50055
 
-type observer struct {
-	id     string
-	server grpc.ServerStreamingServer[informer.Event]
-}
-
-func (o *observer) ID() string {
-	return o.id
-}
-
-func (o *observer) On(event *informer.Event) {
-	if err := o.server.Send(event); err != nil {
-		slog.Error("sending message", "clientID", o.ID(), "error", err)
-	}
-}
-
-// server is used to implement informer.EventStreamServiceServer.
-type server struct {
-	informer.UnimplementedEventStreamServiceServer
-	informers *meta.Informers
-}
-
-func (s *server) Subscribe(msg *informer.SubscribeMessage, server informer.EventStreamService_SubscribeServer) error {
-	// extract peer information to identify it
-	p, ok := peer.FromContext(server.Context())
-	if !ok {
-		return fmt.Errorf("failed to extract peer information")
-	}
-	o := &observer{id: p.Addr.String(), server: server}
-	slog.Info("subscribed component", "id", o.ID())
-	s.informers.Subscribe(o)
-	// Keep the connection open
-	for {
-		select {
-		case <-server.Context().Done():
-			log.Printf("Client %s disconnected", o.ID())
-			s.informers.Unsubscribe(o)
-			return nil
-		}
-	}
-}
-
 func main() {
 	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{AddSource: true, Level: slog.LevelDebug})))
 
-	port := defaultPort
+	ic := service.InformersCache{
+		Port: defaultPort,
+	}
 	portStr := os.Getenv("BEYLA_K8S_CACHE_PORT")
 	if portStr != "" {
 		var err error
-		if port, err = strconv.Atoi(portStr); err != nil {
+		if ic.Port, err = strconv.Atoi(portStr); err != nil {
 			slog.Error("invalid BEYLA_K8S_CACHE_PORT, using default port", "error", err)
-			port = defaultPort
+			ic.Port = defaultPort
 		}
 	}
 
-	infors, err := meta.InitInformers(context.Background(), meta.WithResyncPeriod(30*time.Minute))
-	if err != nil {
-		panic(err)
+	if err := ic.Run(context.Background(),
+		// TODO: make it configurable
+		meta.WithResyncPeriod(30*time.Minute)); err != nil {
+		slog.Error("starting informers' cache service", "error", err)
+		os.Exit(-1)
 	}
-
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
-	panicOnErr(err)
-
-	s := grpc.NewServer()
-	informer.RegisterEventStreamServiceServer(s, &server{informers: infors})
-
-	log.Printf("Server listening on port %d", port)
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
-}
-
-func panicOnErr(err error) {
-	if err != nil {
-		panic(err)
-	}
+	slog.Info("service stopped. Exiting now")
 }
